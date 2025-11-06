dfab5001b83c3cac66233dc58379df87cbb31290
caroline955 <luutrantamanh0905@gmail.com>
2025-11-06 15:28:23 +0700

    hamt

---
+++ hamt systems/create.bat
@@ -0,0 +1,10 @@
+@echo off  
+echo <!DOCTYPE html> > products.html  
+echo <html> >> products.html  
+echo <head> >> products.html  
+echo <title>Products</title> >> products.html  
+echo </head> >> products.html  
+echo <body> >> products.html  
+echo <h1>BIRTHDAY CAKE</h1> >> products.html  
+echo </body> >> products.html  
+echo </html> >> products.html  
